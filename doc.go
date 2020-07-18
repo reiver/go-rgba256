ea5df58cf873336a0141d2aeba578e25780ed3b2
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-18 13:11:56 -0700

    initial commits

---
+++ doc.go
@@ -0,0 +1,5 @@
+/*
+Package rgba256 provides a type that represents an 8-bite RGBA color,
+that seamlessly works with Go's built-in "image", "image/color", and "image/draw" packages.
+*/
+package rgba256
