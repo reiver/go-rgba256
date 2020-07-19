5171bd4dcaad30f39463fcea7f4cb3ebd144739a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-18 17:52:54 -0700

    improved description

--- doc.go
+++ doc.go
@@ -1,5 +1,6 @@
 /*
-Package rgba32 provides a type that represents an 8-bite RGBA color,
+Package rgba32 provides a type that represents an RGBA color with 8-bits per color channel,
+for a total of 32-bits altogether,
 that seamlessly works with Go's built-in "image", "image/color", and "image/draw" packages.
 */
 package rgba32
